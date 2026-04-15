7e5ad901ef6e42e4dada746a7fdfb9fb0284ed02
Hongse0 <hongseyoung981007@gmail.com>
2026-04-15 22:53:29 +0900

    fix(dashboard) : 회원 정보 주입 로직 수정

--- src/main/java/com/sy/side/dashboard/controller/DashboardController.java
+++ src/main/java/com/sy/side/dashboard/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.sy.side.dashboard.controller;
 
+import com.sy.side.common.annotation.UserParam;
+import com.sy.side.common.entity.UserSession;
 import com.sy.side.dashboard.application.port.in.GetDashboardHoldingsUseCase;
 import com.sy.side.dashboard.dto.response.DashboardHoldingsResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,16 +19,17 @@ public class DashboardController {
 
     private final GetDashboardHoldingsUseCase getDashboardHoldingsUseCase;
 
-//    @GetMapping("/holdings")
-//    public DashboardHoldingsResponse getHoldings(
-//            @RequestAttribute("memberId") Long memberId
-//    ) {
-//        return getDashboardHoldingsUseCase.getHoldings(memberId);
-//    }
-
     @GetMapping("/holdings")
-    public DashboardHoldingsResponse getHoldings() {
-        Long memberId = 3L;
+    public DashboardHoldingsResponse getHoldings(
+            @UserParam UserSession userSession
+    ) {
+        Long memberId = userSession.getMemberSession().getMemberId();
         return getDashboardHoldingsUseCase.getHoldings(memberId);
     }
+
+//    @GetMapping("/holdings")
+//    public DashboardHoldingsResponse getHoldings() {
+//        Long memberId = 3L;
+//        return getDashboardHoldingsUseCase.getHoldings(memberId);
+//    }
 }
